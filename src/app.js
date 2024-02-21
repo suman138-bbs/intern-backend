495df5603d6ecc961e5e3f4bff7f0fe65f663941
STSumanBhandari <suman.bhandari@shridharatechnologies.in>
2024-02-21 13:31:30 +0530

    corrected route

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 
-import internshipRoute from "./routes/internShip.route.js";
+import internshipRoute from "./routes/internship.route.js";
 
 export const app = express();
 
